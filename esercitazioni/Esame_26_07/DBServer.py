2e0ed2b4a6793798b34da5c91ebc3fcc3e2d8799
Ae0nix <gianmarcolasalvia@gmail.com>
2025-06-18 13:00:38 +0200

    prova2

--- esercitazioni/Esame_26_07/DBServer.py
+++ esercitazioni/Esame_26_07/DBServer.py
@@ -9,21 +9,48 @@ def get_database():
 
 @app.put("/create_booking")
 def create_booking():
+
     db = get_database()
     collection = db["booking_collection"]
 
     request_message = request.get_json()
     try:
         collection.insert_one(request_message)
-    except:
-        
-    return "ACK", 200
+    except Exception as e:
+        print("[DBSERVER] Operation failed")
+        return "Fail-"+str(e), 500
+    else:
+        print("[DBSERVER] Booking insert correctly")
+        return "ACK", 200
+    
+
+@app.post("/update_booking")
+def update_booking():
+
+    db = get_database()
+    collection = db["booking_collection"]
+
+    request_message = request.get_json()
+
+    operator = request_message["operator"]
+
+
+    list_of_bookings = list(collection.find({
+        "operator":operator, 
+        "nights": {"$gte": request_message["nights"]}
+        }))
 
 
 
 
+    try:
 
-
+    except Exception as e:
+        print("[DBSERVER] Operation failed")
+        return "Fail-"+str(e), 500
+    else:
+        print("[DBSERVER] Booking updated correctly")
+        return "ACK", 200
 
 
 
